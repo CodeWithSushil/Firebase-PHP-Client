303595947056ba5a96a723d7c8bbd5fa13ef5f86
Sushil Kumar <sushilkumbhar77@gmail.com>
2024-08-16 14:33:18 +0530

    v1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# Firebase-Client
-Communicate to Firebase Realtime Database.
+## Firebase-Client
+Firebase Client for PHP.
+Create your PHP project with firebase's Realtime Database.
+
+### Example
+```php
+<?php
+
+require "vendor/autoload.php";
+use Firebase\Firebase;
+
+$url = "your firebase url";
+
+$client = new Firebase($url);
+
+// Read data
+$client->retrieve("users");
+
+// Post data
+$data = ['name' => 'John doe', 'age' => 28];
+$client->insert("users", $data);
+
+// Update data
+$data = ['name' => 'Natasha', 'age' => 21];
+$client->insert("users", $data, $id);
+
+// Delete data
+$client->update("users", $id);
+```
--- src/Firebase.php
+++ src/Firebase.php
@@ -1,35 +1,33 @@
 <?php
-/*
- * class name: firebaseRDB
- * version: 1.0
- * author: Devisty
- */
+namespace Firebase\Firebase;
 
-class firebaseRDB{
-   function __construct($url=null) {
-      if(isset($url)){
-         $this->url = $url;
-      }else{
-         throw new Exception("Database URL must be specified");
-      }
-   }
-
-   public function grab($url, $method, $par=null){
-      $ch = curl_init();
-      curl_setopt($ch, CURLOPT_URL, $url);
-      curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-      if(isset($par)){
-         curl_setopt($ch, CURLOPT_POSTFIELDS, $par);
-      }
-      curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
-      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-      curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-      curl_setopt($ch, CURLOPT_TIMEOUT, 120);
-      curl_setopt($ch, CURLOPT_HEADER, 0);
-      $html = curl_exec($ch);
-      return $html;
-      curl_close($ch);
-   }
+class Firebase
+{
+  function __construct($url=null){
+    if(isset($url)){
+      $this->url = $url;
+    } else {
+      throw new Exception("Database URL must be specified");
+    }
+  }
+  
+  public function grab($url, $method, $par=null){
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    
+    if(isset($par)){
+      curl_setopt($ch, CURLOPT_POSTFIELDS, $par);
+    }
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 120);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    $html = curl_exec($ch);
+    return $html;
+    curl_close($ch);
+  }
 
 
    public function insert($table, $data){
