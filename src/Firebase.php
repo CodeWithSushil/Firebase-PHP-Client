7b7185633ea174e00cc08c6d063e2c719957db0f
Sushil Kumar <sushilkumbhar77@gmail.com>
2024-09-15 15:27:58 +0530

    update and code optimize

--- src/Firebase.php
+++ src/Firebase.php
@@ -1,66 +1,96 @@
 <?php
 namespace Firebase\Firebase;
+use Exception;
 
-class Firebase{
+class Firebase
+{
   private $url;
-  function __construct($url=null){
-    if(isset($url)){
+  
+  function __construct($url=null)
+  {
+    if(isset($url))
+    {
       $this->url = $url;
-    } else {
+    }
+    else
+    {
       throw new Exception("Database URL must be specified");
     }
   }
   
-  public function grab($url, $method, $par=null){
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  public function grab($url, $method, $par=null)
+  {
+    $headers = [
+      "Content-Type: application/json"
+    ];
+
+    $options = [
+      CURLOPT_URL => $url,
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_HEADER => true,
+      CURLOPT_SSL_VERIFYPEER => false,
+      CURLOPT_FOLLOWLOCATION => true,
+      CURLOPT_CUSTOMREQUEST => $method,
+      CURLOPT_TIMEOUT => 120,
+      CURLOPT_HTTPHEADER => $headers
+    ];
     
+    $ch = curl_init();
+    curl_setopt_array($ch, $options);
+
     if(isset($par)){
       curl_setopt($ch, CURLOPT_POSTFIELDS, $par);
     }
-    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-    curl_setopt($ch, CURLOPT_TIMEOUT, 120);
-    curl_setopt($ch, CURLOPT_HEADER, 0);
+
     $html = curl_exec($ch);
     return $html;
+
+    if(curl_errno($ch)){
+      echo "Error: ". curl_error($ch);
+    }
     curl_close($ch);
   }
+  
+  public function insert($table, $data)
+  {
+    $path = $this->url."/$table.json";
+    $grab = $this->grab($path, "POST", json_encode($data));
+    return $grab;
+  }
 
-
-   public function insert($table, $data){
-      $path = $this->url."/$table.json";
-      $grab = $this->grab($path, "POST", json_encode($data));
-      return $grab;
-   }
-
-   public function update($table, $uniqueID, $data){
-      $path = $this->url."/$table/$uniqueID.json";
-      $grab = $this->grab($path, "PATCH", json_encode($data));
-      return $grab;
-   }
-
-   public function delete($table, $uniqueID){
-      $path = $this->url."/$table/$uniqueID.json";
-      $grab = $this->grab($path, "DELETE");
-      return $grab;
-   }
-
-   public function retrieve($dbPath, $queryKey=null, $queryType=null, $queryVal =null){
-      if(isset($queryType) && isset($queryKey) && isset($queryVal)){
-         $queryVal = urlencode($queryVal);
-         if($queryType == "EQUAL"){
-               $pars = "orderBy=\"$queryKey\"&equalTo=\"$queryVal\"";
-         }elseif($queryType == "LIKE"){
-               $pars = "orderBy=\"$queryKey\"&startAt=\"$queryVal\"";
-         }
+  public function update($table, $uniqueID, $data)
+  {
+    $path = $this->url."/$table/$uniqueID.json";
+    $grab = $this->grab($path, "PATCH", json_encode($data));
+    return $grab;
+  }
+  
+  public function delete($table, $uniqueID)
+  {
+    $path = $this->url."/$table/$uniqueID.json";
+    $grab = $this->grab($path, "DELETE");
+    return $grab;
+  }
+  
+  public function retrieve($dbPath, $queryKey=null, $queryType=null, $queryVal =null)
+  {
+    if(isset($queryType) && isset($queryKey) && isset($queryVal))
+    {
+      $queryVal = urlencode($queryVal);
+      
+      if($queryType == "EQUAL")
+      {
+        $pars = "orderBy=\"$queryKey\"&equalTo=\"$queryVal\"";
       }
-      $pars = isset($pars) ? "?$pars" : "";
-      $path = $this->url."/$dbPath.json$pars";
-      $grab = $this->grab($path, "GET");
-      return $grab;
-   }
-
+      elseif($queryType == "LIKE")
+      {
+        $pars = "orderBy=\"$queryKey\"&startAt=\"$queryVal\"";
+      }
+    }
+    
+    $pars = isset($pars) ? "?$pars" : "";
+    $path = $this->url."/$dbPath.json$pars";
+    $grab = $this->grab($path, "GET");
+    return $grab;
+  }
 }
